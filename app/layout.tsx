e153a76c0cac960e01641bfa8863265d2d27e70d
luanmessias <luanmessias03@gmail.com>
2025-12-08 11:34:07 +0000

    finish base configuration and linting

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 
-const a = { b: 1 }
-
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
